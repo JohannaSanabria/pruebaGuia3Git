bb55236c46de265034c2d5253e346b3c1c2f8066
FrancoAlcaraz <franco231000@gmail.com>
2023-08-09 11:42:20 -0300

    suma de la rama anterior

---
+++ src/pruebaguia3git/clasefranco.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebaguia3git;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Usuario
+ */
+public class clasefranco {
+      int suma=0 ;
+    public int suma(){
+        Scanner sc=new Scanner(System.in);
+        int f=0;
+        for (int i = 0; i < f; i++) {
+            System.out.println("Ingrese Limite de Datos");
+            f=sc.nextInt();
+            suma=suma+i;            
+       
+        }
+        return suma;
+
+    }
+}
