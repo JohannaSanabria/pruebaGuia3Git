0625aebeabbbec839ae7fd88e01660582bc81eff
LucPach <lp17051994@gmail.com>
2023-08-09 11:50:48 -0300

    Class Lu
    Nueva clase

---
+++ src/pruebaguia3git/ClassLu.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebaguia3git;
+
+/**
+ *
+ * @author Lucy
+ */
+public class ClassLu {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String [] arraycito= new String [10];
+    
+    for (int i = 0; i<arraycito.length ;i++){
+      arraycito[i]="git humberto";
+        System.out.println(arraycito[i]+"Es un poquito complicado ");
+      
+    }
+    
+    
+    
+    
+    
+    }
+}
+    
