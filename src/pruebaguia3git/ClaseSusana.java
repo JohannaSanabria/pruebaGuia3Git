6c78af69a562c364f53dd6bd28034c4b14bf7bd9
Susana <su_goro@hotmail.com>
2023-08-09 11:39:24 -0300

    hice la clase susana

---
+++ src/pruebaguia3git/ClaseSusana.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebaguia3git;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ClaseSusana {
+   private String frase; 
+
+    public ClaseSusana(String frase) {
+        this.frase = frase;
+    }
+
+    public String getFrase() {
+        return frase;
+    }
+
+    public void setFrase(String frase) {
+        this.frase = frase;
+    }
+  
+   
+}
